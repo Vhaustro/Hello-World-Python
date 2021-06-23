36c6f9f13193b121979450f1b37c4a8e5d6e8d88
unknown <mandenno228@gmail.com>
2021-06-23 10:37:44 +0300

    update

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+#Print Hello World
+print ('Hello World')
+
+#Single and Double Quotes
+print ('Hello World in Single Quotes')
+print ("Hello World in Double Quotes")
+
+#My Intro (Getting info from user)
+print('Welcome To my Python Interpretor VSC')
+print ()
+name = input('What is your name')
+print('How are you doing?')
+print(name)
+print()
+print('Please Enter Your Name:')
+print(name)
+
