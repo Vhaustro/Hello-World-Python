2f0356823532b4f845e62c98967d91319b5ec0a5
unknown <mandenno228@gmail.com>
2021-06-27 23:52:53 +0300

    update

---
+++ erro-h.py
@@ -0,0 +1,5 @@
+#Error-handling is a situation of which occues when a problem arises but is not of knowledge to the User
+#Debugging is when the User knows there's a problem with the code from extenal file into the main directory
+
+#The two are usually intertwined with eachother within the concept of fixing code.
+
